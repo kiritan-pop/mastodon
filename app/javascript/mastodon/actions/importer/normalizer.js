20380e5a126b37cba93e79ad72e48c6cf327c489
kiritan-pop <kiritan.pop@gmail.com>
2019-05-02 21:44:46 +0900

    rubber band animation

--- app/javascript/mastodon/actions/importer/normalizer.js
+++ app/javascript/mastodon/actions/importer/normalizer.js
@@ -34,9 +34,8 @@ const avatarEmojify = (text, avatar_emojis) => {
 
 const kiriAminefy = (text) => {
   var tmp_content = text;
-  var match_result = tmp_content.match(/(\[\[\[.+\]\]\])/g);
+  var match_result = tmp_content.match(/(\[\[\[[^\]]+\]\]\])/g);
   if (match_result && match_result.length > 0){
-    match_result = Array.from(new Set(match_result));
     for (let p of match_result) {
       var replacement = `<span class="rubberband"><span>${p.slice(3,-3)}</span></span>`;
       tmp_content = tmp_content.replace(p, replacement);  
