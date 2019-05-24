b56b087f5193ab6ad993ae77503f7dd17a120302
kiritan-pop <kiritan.pop@gmail.com>
2019-05-24 14:47:14 +0900

    emoji flip

--- app/javascript/mastodon/actions/importer/normalizer.js
+++ app/javascript/mastodon/actions/importer/normalizer.js
@@ -19,17 +19,24 @@ const kiriAminefy = (text) => {
           tmp_content = tmp_content.replace(p, replacement);
       }
   }
-var match_result = tmp_content.match(/(\[\[\[[^\]]+\]\]\])|(［［［[^］]+］］］)/g);
+  var match_result = tmp_content.match(/(\[\[\[[^\]]+\]\]\])|(［［［[^］]+］］］)/g);
   if (match_result && match_result.length > 0){
     for (let p of match_result) {
       var replacement = `<span class="spin"><span>${p.slice(3,-3)}</span></span>`;
       tmp_content = tmp_content.replace(p, replacement);  
     }
   }
-  var match_result = tmp_content.match(/(\{\{\{[^\}]+\}\}\})|(\｛｛｛[^｝]+｝｝｝)/g);
+  var match_result = tmp_content.match(/(\{\{\{[^\}]+\}\}\})|(｛｛｛[^｝]+｝｝｝)/g);
   if (match_result && match_result.length > 0){
     for (let p of match_result) {
       var replacement = `<span class="jump"><span>${p.slice(3,-3)}</span></span>`;
+      tmp_content = tmp_content.replace(p, replacement);  
+    }
+  }
+  var match_result = tmp_content.match(/(<<<[^>]+>>>)|(＜＜＜[^＞]+＞＞＞)/g);
+  if (match_result && match_result.length > 0){
+    for (let p of match_result) {
+      var replacement = `<span class="flip"><span>${p.slice(3,-3)}</span></span>`;
       tmp_content = tmp_content.replace(p, replacement);  
     }
   }
