106f5af5b7e927422a5f013eced0e0ddd62be931
kiritan-pop <kiritan.pop@gmail.com>
2020-04-05 08:44:21 +0900

    update

--- config/initializers/source.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module Mastodon
-    module Version
-      module_function
-      def source_base_url
-        'https://github.com/kiritan-pop/mastodon'
-      end
-    end
-  end
