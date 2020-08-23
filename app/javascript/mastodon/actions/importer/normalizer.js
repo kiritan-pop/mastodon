4285aa6e15a34d866881474e2524e1cfa175125d
kiritan-pop <kiritan.pop@gmail.com>
2020-08-23 16:25:43 +0900

    update kiri_anime 多重括弧対応

--- app/javascript/mastodon/actions/importer/normalizer.js
+++ app/javascript/mastodon/actions/importer/normalizer.js
@@ -12,41 +12,51 @@ const makeEmojiMap = record => record.all_emojis.reduce((obj, emoji) => {
 
 const kiriAminefy = (text) => {
   var tmp_content = text;
-  var match_result = tmp_content.match(/(\(\(\([^\)]+\)\)\))|(（（（[^）]+）））)/g);
-  if (match_result && match_result.length > 0) {
+  var loop = true
+  while(loop){
+    loop = false
+    var match_result = tmp_content.match(/(\(\(\([^\)]+\)\)\))|(（（（[^）]+）））)/g);
+    if (match_result && match_result.length > 0) {
+      loop = true
       for (let p of match_result) {
-          var replacement = `<span class="rubberband"><span>${p.slice(3, -3)}</span></span>`;
-          tmp_content = tmp_content.replace(p, replacement);
+        var replacement = `<span class="rubberband"><span>${p.slice(3, -3)}</span></span>`;
+        tmp_content = tmp_content.replace(p, replacement);
       }
-  }
-  var match_result = tmp_content.match(/(\[\[\[[^\]]+\]\]\])|(［［［[^］]+］］］)/g);
-  if (match_result && match_result.length > 0){
-    for (let p of match_result) {
-      var replacement = `<span class="spin"><span>${p.slice(3,-3)}</span></span>`;
-      tmp_content = tmp_content.replace(p, replacement);  
+    }
+    var match_result = tmp_content.match(/(\[\[\[[^\]]+\]\]\])|(［［［[^］]+］］］)/g);
+    if (match_result && match_result.length > 0) {
+      loop = true
+      for (let p of match_result) {
+        var replacement = `<span class="spin"><span>${p.slice(3, -3)}</span></span>`;
+        tmp_content = tmp_content.replace(p, replacement);
+      }
+    }
+    var match_result = tmp_content.match(/(\{\{\{[^\}]+\}\}\})|(｛｛｛[^｝]+｝｝｝)/g);
+    if (match_result && match_result.length > 0) {
+      loop = true
+      for (let p of match_result) {
+        var replacement = `<span class="jump"><span>${p.slice(3, -3)}</span></span>`;
+        tmp_content = tmp_content.replace(p, replacement);
+      }
+    }
+    var match_result = tmp_content.match(/(＜＜＜[^＞]+＞＞＞)/g);
+    if (match_result && match_result.length > 0) {
+      loop = true
+      for (let p of match_result) {
+        var replacement = `<span class="flip"><span>${p.slice(3, -3)}</span></span>`;
+        tmp_content = tmp_content.replace(p, replacement);
+      }
+    }
+    var match_result = tmp_content.match(/(「「「[^」]+」」」)/g);
+    if (match_result && match_result.length > 0) {
+      loop = true
+      for (let p of match_result) {
+        var replacement = `<span class="flip-v"><span>${p.slice(3, -3)}</span></span>`;
+        tmp_content = tmp_content.replace(p, replacement);
+      }
     }
   }
-  var match_result = tmp_content.match(/(\{\{\{[^\}]+\}\}\})|(｛｛｛[^｝]+｝｝｝)/g);
-  if (match_result && match_result.length > 0){
-    for (let p of match_result) {
-      var replacement = `<span class="jump"><span>${p.slice(3,-3)}</span></span>`;
-      tmp_content = tmp_content.replace(p, replacement);  
-    }
-  }
-  var match_result = tmp_content.match(/(＜＜＜[^＞]+＞＞＞)/g);
-  if (match_result && match_result.length > 0){
-    for (let p of match_result) {
-      var replacement = `<span class="flip"><span>${p.slice(3,-3)}</span></span>`;
-      tmp_content = tmp_content.replace(p, replacement);  
-    }
-  }
-  var match_result = tmp_content.match(/(「「「[^」]+」」」)/g);
-  if (match_result && match_result.length > 0) {
-    for (let p of match_result) {
-      var replacement = `<span class="flip-v"><span>${p.slice(3, -3)}</span></span>`;
-      tmp_content = tmp_content.replace(p, replacement);
-    }
-  }
+
   return tmp_content;
 }
 
