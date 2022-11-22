3b9a2ca557435698d5f1ed5bb608ddbd2d4585bd
kiritan-pop <kiritan.pop@gmail.com>
2022-11-22 23:02:08 +0900

    Revert "デバッグ用情報出力"
    This reverts commit 3a21ed69a7eb05aabe0df6ccdd73f7a1b6c57ce1.

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -348,8 +348,6 @@ class ComposeForm extends ImmutablePureComponent {
             className='spoiler-input__input'
           />
         </div>
-        {console.log("**** debug ****")}
-        {console.log(this.props.text)}
         <AutosuggestTextarea
           ref={this.setAutosuggestTextarea}
           placeholder={intl.formatMessage(messages.placeholder)}
